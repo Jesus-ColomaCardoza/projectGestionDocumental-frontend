0d5998ee989f0910476963bbdb70643b8381fe58
Jesus Coloma <colomacardoza16@gmail.com>
2023-10-12 22:52:55 -0500

    crud-user: user delete already works and I added sweetalerts js

--- src/pages/Usuario.jsx
+++ src/pages/Usuario.jsx
@@ -5,20 +5,66 @@ import { Link } from 'react-router-dom';
 function Usuario() {
 
   const [users, setUsers] = useState([]);
+  const [userName, setUserName] = useState('');
 
   useEffect(() => { loadUsers() }, []);
+  useEffect(() => { loadUsersByName() }, [userName]);
 
   const loadUsers = async () => {
     const response = await fetch('http://localhost:3000/usuario/getlist');
     const data = await response.json()
     setUsers(data);
-    console.log(data);
+    //console.log(data);
   }
 
+  const loadUsersByName = async () => {
+    try {
+      if (userName !== '') {
+        const response = await fetch("http://localhost:3000/usuario/getbyname/" + userName);
+        const data = await response.json();
+        setUsers(data);
+        //console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  const handleBuscarUserName = (e) => {
+    const name = e.target.value;
+    setUserName(name)
+    if (name == '') {
+      loadUsers()
+    }
+  }
 
-
-
+  const deleteUser = async (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: '¿Está seguro de eliminar este usuario?',
+      text: "¡No podrá revertir!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#28A745',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await fetch("http://localhost:3000/usuario/delete/" + id, {
+          method: "DELETE",
+        });
+        setUsers(users.filter(user => user.id !== id));
+        Swal.fire({
+            title: '¡Eliminación exitosa!',
+            text: 'El usuario has sido eliminado',
+            icon: 'success',
+            confirmButtonColor: '#28A745',
+            confirmButtonText: 'Ok'
+          }
+        )
+      }
+    })
+  }
   return (
 
     <div className='container'>
@@ -26,7 +72,17 @@ function Usuario() {
         <h2 className='container-list__h2'>Mantenimiento de usuario</h2>
       </header>
       <div className='container-list'>
-        <h3 className='container-list__h3'>Listado de Usuario</h3>
+
+        <div className='container-list__header'>
+          <h3 className='container-list__h3'>Listado de Usuario</h3>
+          <button type="button" className="btn btn-success me-1">
+            <Link>
+              <i className="bi bi-plus text--white"></i>
+            </Link>
+            Nuevo Registro
+          </button>
+        </div>
+
         <div>
 
           <nav className="navbar navbar-expand-lg">
@@ -41,10 +97,16 @@ function Usuario() {
                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
                 </ul>
-                <form className="d-flex" role="search">
-                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                  <button className="btn btn-success" type="submit">Search</button>
-                </form>
+                <div className="d-flex" role="search">
+                  <label className="my-auto">Buscar:</label>
+                  <input
+                    className="form-control mx-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={handleBuscarUserName}
+                    value={userName} />
+                </div>
               </div>
             </div>
           </nav>
@@ -71,18 +133,38 @@ function Usuario() {
                         <td>{user.user_name}</td>
                         <td>{user.Area.area_name}</td>
                         <td>{user.role}</td>
-                        <td>{user.Area.state}</td>
-                        <td>{user.state}</td>
+                        <td>{
+                          user.Empleado.employee_name + ' ' +
+                          user.Empleado.paternal_surname + ' ' +
+                          user.Empleado.maternal_surname
+                        }
+                        </td>
                         <td>
-                          <button type="button" className="btn btn-info mx-1">
+                          <span className='highlighter highlighter--red'>
+                            {user.state}
+                          </span>
+                        </td>
+                        <td>
+                          <button type="button" className="btn btn-info me-1">
                             <Link>
                               <i className="bi bi-pencil-fill text--white"></i>
                             </Link>
                           </button>
-                          <button type="button" className="btn btn-danger mx-1">
+                          <button
+                            type="button"
+                            className="btn btn-warning me-1"
+                            onClick={() => { deleteUser(user.id) }}>
+                            <i className="bi bi-trash-fill text--white"></i>
+                          </button>
+                          <button type="button" className="btn btn-success me-1">
                             <Link>
-                              <i className="bi bi-trash-fill text--white"></i>
-                            </Link>    
+                              <i className="bi bi-check-circle-fill text--white"></i>
+                            </Link>
+                          </button>
+                          <button type="button" className="btn btn-danger me-1">
+                            <Link>
+                              <i className="bi bi-x-circle-fill text--white"></i>
+                            </Link>
                           </button>
                         </td>
                       </tr>
